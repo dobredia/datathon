3c9e4dcbbd61d3cb8b48b4c1a9f0c74b39e2df16
Vlad Danaila <vlad.danaila@ro.ibm.com>
2018-09-28 22:58:36 +0300

    File reading

---
+++ data-loading/process-air-quality-official.py
@@ -0,0 +1,32 @@
+import codecs
+
+'''
+    0.  Countrycode,
+    1.  Namespace,
+    2.  AirQualityNetwork,
+    3.  AirQualityStation,
+    4.  AirQualityStationEoICode,
+    5.  SamplingPoint,
+    6.  SamplingProcess,
+    7.  Sample,
+    8.  AirPollutant,
+    9.  AirPollutantCode,
+    10. AveragingTime,
+    11. Concentration,
+    12. UnitOfMeasurement,
+    13. DatetimeBegin,
+    14. DatetimeEnd,
+    15. Validity,
+    16. Verification
+'''
+
+def rewrite_lines(new_file_path = '../../datathlon data/air-quality-official/Processed_BG_5_9421_2013_timeseries.csv', file_to_read_path = '../../datathlon data/air-quality-official/BG_5_9421_2013_timeseries.csv'):
+    new_file = open(new_file_path, "w")
+    f = codecs.open(file_to_read_path, "r", "utf-16")
+    lines = f.readlines()
+    for line in lines:
+        split = line.split(',')
+        print(split)
+
+if __name__ == "__main__":
+    rewrite_lines()
