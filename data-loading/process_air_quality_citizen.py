163b1f4345bd74a796371db4c2c9b760c0ddf7b3
dobredia <dobredia@yahoo.com>
2018-09-28 14:56:23 -0700

    citizen sample with header

--- data-loading/process_air_quality_citizen.py
+++ data-loading/process_air_quality_citizen.py
@@ -11,8 +11,27 @@ from geohash.geohashdecode import decode1
     6.  pressure
 '''
 
-def rewrite_lines(new_file_path = '../../datathlon data/air-quality-citizen/Processed_sample_data_bg_2018.csv', file_to_read_path = '../../datathlon data/air-quality-citizen/sample_data_bg_2018.csv'):
+def rewrite_lines_0(new_file_path = '../../datathlon data/air-quality-citizen/Processed_sample_data_bg_2018.csv', file_to_read_path = '../../datathlon data/air-quality-citizen/sample_data_bg_2018.csv'):
     new_file = open(new_file_path, "w")
+    f = codecs.open(file_to_read_path, "r", "utf-8")
+    lines = f.readlines()
+
+    for line in lines[:1]:
+        split = line.split(',')
+        array = [
+            split[0], # time
+            'lon', # geohash / lon
+            'lat', # geohash / lat
+            #split[2], # P1 PM10
+            split[3], # P2 PM2.5
+            split[4], # temperature
+            split[5],  # humidity
+            split[6][:-1],  # pressure
+        ]
+        new_file.write(','.join(array))
+
+def rewrite_lines(new_file_path = '../../datathlon data/air-quality-citizen/Processed_sample_data_bg_2018.csv', file_to_read_path = '../../datathlon data/air-quality-citizen/sample_data_bg_2018.csv'):
+    new_file = open(new_file_path, "a")
     f = codecs.open(file_to_read_path, "r", "utf-8")
     lines = f.readlines()
 
@@ -23,7 +42,7 @@ def rewrite_lines(new_file_path = '../../datathlon data/air-quality-citizen/Proc
             split[0], # time
             str(lon), # geohash / lon
             str(lat), # geohash / lat
-            split[2], # P1 PM10
+            #split[2], # P1 PM10
             split[3], # P2 PM2.5
             split[4], # temperature
             split[5],  # humidity
@@ -32,4 +51,5 @@ def rewrite_lines(new_file_path = '../../datathlon data/air-quality-citizen/Proc
         new_file.write(','.join(array))
 
 if __name__ == "__main__":
+    rewrite_lines_0()
     rewrite_lines()
