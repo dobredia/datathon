66a6ccf02d00c6779d6755892e6f023d4037bd05
Vlad Danaila <vlad.danaila@ro.ibm.com>
2018-09-29 18:55:21 +0300

    heatmap for both citizen and official

--- heatmap/heatmap_citizen_official.py
+++ heatmap/heatmap_citizen_official.py
@@ -6,26 +6,6 @@ from data_loading.process_air_quality_official import data_for_heatmap as data_f
 if __name__ == "__main__":
 
     '''
-        CITIZEN DATA
-    '''
-
-    data_citizen = pd.read_csv('../../datathlon data/air-quality-citizen/Processed_heatmap_all_citizen.csv')
-    # print(data.head())
-    # print(data.shape)
-    # print(data.columns)
-    couple_columns_citizen = data_citizen[['Concentration', 'Longitude', 'Latitude']]
-    # print(couple_columns.head())
-    # print(data.ix[:, ['Concentration', 'Longitude', 'Latitude']].head())
-    data_lat_long_citizen = couple_columns_citizen.groupby(['Latitude', 'Longitude']).mean()
-    # print(data_lat_long.shape)
-    # print(data_lat_long.head(10))
-
-    data_lat_long_citizen = data_lat_long_citizen.reset_index()
-    # print(data_lat_long.head())
-    # major_ticks = np.arange(0.2, 0.5, 0.01)
-    # minor_ticks = np.arange(0, 50, 1)
-
-    '''
         OFFICIAL DATA
     '''
 
@@ -41,6 +21,26 @@ if __name__ == "__main__":
     # print(data_lat_long.head(10))
 
     data_lat_long_official = data_lat_long_official.reset_index()
+    # print(data_lat_long.head())
+    # major_ticks = np.arange(0.2, 0.5, 0.01)
+    # minor_ticks = np.arange(0, 50, 1)
+
+    '''
+        CITIZEN DATA
+    '''
+
+    data_citizen = pd.read_csv('../../datathlon data/air-quality-citizen/Processed_heatmap_all_citizen.csv')
+    # print(data.head())
+    # print(data.shape)
+    # print(data.columns)
+    couple_columns_citizen = data_citizen[['Concentration', 'Longitude', 'Latitude']]
+    # print(couple_columns.head())
+    # print(data.ix[:, ['Concentration', 'Longitude', 'Latitude']].head())
+    data_lat_long_citizen = couple_columns_citizen.groupby(['Latitude', 'Longitude']).mean()
+    # print(data_lat_long.shape)
+    # print(data_lat_long.head(10))
+
+    data_lat_long_citizen = data_lat_long_citizen.reset_index()
     # print(data_lat_long.head())
     # major_ticks = np.arange(0.2, 0.5, 0.01)
     # minor_ticks = np.arange(0, 50, 1)
@@ -64,9 +64,16 @@ if __name__ == "__main__":
     ax.set_ylabel('Longitude', fontsize = 10);
     ax.set_title('Concentration', size = 15)
 
+    clip_config = {
+        'Concentration': data_lat_long_official['Concentration'].max(),
+        'Latitude': data_lat_long_citizen['Latitude'].max(),
+        'Longitude': data_lat_long_citizen['Longitude'].max()
+    }
+    data_lat_long_citizen = data_lat_long_citizen.clip(upper = pd.Series(clip_config), axis=1)
+
     cbar = plt.colorbar(mappable = s_1, ax = ax)
 
     s_2 = ax.scatter('Latitude', 'Longitude', c='Concentration', data=data_lat_long_citizen, cmap='RdYlGn_r', marker='.', s=50)
-    cbar = plt.colorbar(mappable=s_2, ax=ax)
+    # cbar = plt.colorbar(mappable=s_2, ax=ax)
 
     plt.show()
