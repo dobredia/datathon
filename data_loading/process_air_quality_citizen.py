e9fca8f1c3832ac6b1b0f3a3cdba4cced0726462
Vlad Danaila <vlad.danaila@ro.ibm.com>
2018-09-29 17:16:37 +0300

    heatmap citizen

--- data_loading/process_air_quality_citizen.py
+++ data_loading/process_air_quality_citizen.py
@@ -1,3 +1,5 @@
+from os import listdir
+from os.path import isfile, join
 import codecs
 from geohash.geohashdecode import decode1
 from data_loading.data_load_utils import time_hash, process_date_str
@@ -56,16 +58,22 @@ def rewrite_lines_for_heat_map(
         file_to_read_path = '../../datathlon data/air-quality-citizen/sample_data_bg_2018.csv',
         write_mode = 'w',
         include_header = False):
+
     new_file = open(new_file_path, write_mode)
     f = codecs.open(file_to_read_path, "r", "utf-8")
     lines = f.readlines()
     if include_header:
         new_file.write('DatetimeEndHash,Longitude,Latitude,Concentration\n')
+    count = 0
     for line in lines[1:]:
         split = line.split(',')
         year, month, day, hour = process_date_str(split[0])
         _time_hash = time_hash(year, month, day, hour)
-        lon, lat = decode1(split[1])
+        try:
+            lon, lat = decode1(split[1])
+        except:
+            print('skipped record because can not decode location hash : ', split[1])
+            continue
         pm_10 = split[2]
         array = [
             str(_time_hash),
@@ -74,8 +82,20 @@ def rewrite_lines_for_heat_map(
             pm_10 + '\n'
         ]
         new_file.write(','.join(array))
+        count += 1
+        if count % 100000 == 0:
+            print(count / len(lines) * 100, '%')
+
+def merge_all_files_for_heatmap(
+        new_file_path = '../../datathlon data/air-quality-citizen/Processed_heatmap_all_citizen.csv',
+        file_paths = ['../../datathlon data/air-quality-citizen/data_bg_2017.csv/data_bg_2017.csv', '../../datathlon data/air-quality-citizen/data_bg_2018.csv/data_bg_2018.csv']):
+    new_file = open(new_file_path, 'a')
+    new_file.write('DatetimeEndHash,Longitude,Latitude,Concentration\n')
+    for path in file_paths:
+        rewrite_lines_for_heat_map(new_file_path = new_file_path, file_to_read_path = path, write_mode = 'a')
 
 if __name__ == "__main__":
     # rewrite_lines_0()
     # rewrite_lines()
-    rewrite_lines_for_heat_map(include_header = True)
+    # rewrite_lines_for_heat_map(include_header = True)
+    merge_all_files_for_heatmap()
---
+++ heatmap/heatmap_station_citizen.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from data_loading.process_air_quality_official import data_for_heatmap as data_for_heatmap_official
+
+if __name__ == "__main__":
+
+    data = pd.read_csv('../../datathlon data/air-quality-citizen/Processed_heatmap_all_citizen.csv')
+    # print(data.head())
+    # print(data.shape)
+    # print(data.columns)
+    couple_columns = data[['Concentration', 'Longitude', 'Latitude']]
+    # print(couple_columns.head())
+    # print(data.ix[:, ['Concentration', 'Longitude', 'Latitude']].head())
+    data_lat_long = couple_columns.groupby(['Latitude', 'Longitude']).mean()
+    # print(data_lat_long.shape)
+    # print(data_lat_long.head(10))
+
+    data_lat_long = data_lat_long.reset_index()
+    # print(data_lat_long.head())
+    # major_ticks = np.arange(0.2, 0.5, 0.01)
+    # minor_ticks = np.arange(0, 50, 1)
+
+    fig = plt.figure(figsize=(6, 5))
+    ax = fig.add_subplot(1, 1, 1)
+    s = ax.scatter('Latitude', 'Longitude', c = 'Concentration', data = data_lat_long, cmap = 'Reds', marker = 's', s = 190)
+    ax.axis([
+        data_lat_long['Latitude'].min() - 0.05,
+        data_lat_long['Latitude'].max() + 0.05,
+        data_lat_long['Longitude'].min() - 0.05,
+        data_lat_long['Longitude'].max() + 0.05
+    ])
+    # ax.set_xticks(major_ticks)
+    # ax.set_xticks(minor_ticks, minor = True)
+    # ax.set_yticks(major_ticks)
+    ax.grid(which='both', alpha = 0.3)
+    ax.grid(which='major', alpha = 0.3)
+    ax.set_xlabel('Latitude', fontsize = 10);
+    ax.set_ylabel('Longitude', fontsize = 10);
+    ax.set_title('Concentration', size = 15)
+
+    cbar = plt.colorbar(mappable = s, ax = ax)
+
+    plt.show()
+
